4e33fc4d4839a6f968d1ae29bca9a890cd11112b
trailovic <trailovicluka@outlook.com>
2026-02-23 12:02:03 +0100

    Fix spacing for narrow screens and adjust button colours

--- src/lib/components/Title.svelte
+++ src/lib/components/Title.svelte
@@ -2,7 +2,7 @@
     import Logo from '$lib/assets/logo.png';
 </script>
 
-<div class="flex items-center gap-2 md:gap-4 my-2 md:my-6">
+<div class="flex items-center gap-2 md:gap-4 my-4 md:my-6">
     <img src={Logo} alt="Logo" class="w-8 h-8 md:w-10 md:h-10" />
     <h1 class="text-2xl md:text-3xl font-bold">My Task App</h1>
 </div>
